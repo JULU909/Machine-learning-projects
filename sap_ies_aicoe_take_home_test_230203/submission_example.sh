784b00be21fb5346f70e1199bd874712b0075b47
Harish Vasanth <94796213+JULU909@users.noreply.github.com>
2023-05-31 02:48:54 +0800

    file formatting

--- sap_ies_aicoe_take_home_test_230203/submission_example.sh
+++
@@ -1,2 +0,0 @@
-# please replace the folder path with your own path
-azcopy copy "/home/projects/john_210131" "https://hiringstorageacc.blob.core.windows.net/hiring-artefacts/submissions?sp=w&st=2023-01-26T02:24:19Z&se=2024-02-01T00:24:19Z&spr=https&sv=2021-06-08&sr=d&sig=y1lAiu69Ps%2BowTayVtTiOA0ftQ0QpA83ByrGAin8N3c%3D&sdd=1" --recursive=true
